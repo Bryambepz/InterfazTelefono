14ad96cb42d807e0202b1e5387fb4ff22e1ca195
braya <braya@BRYEPZ>
2020-06-13 16:36:53 -0500

    Instaciar los controladores

---
+++ test/ec/edu/ups/vista/VistaTelefono.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.vista;
+
+import ec.edu.ups.modelo.Telefono;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author braya
+ */
+public class VistaTelefono {
+    private Scanner l;
+
+    public VistaTelefono() {
+        l=new Scanner(System.in);
+    }
+    
+    public Telefono agregarTelefono(){
+        int codigo = l.nextInt();
+        int numero = l.nextInt();
+        String tipo=l.next();
+        String operadora = l.next();
+        return new Telefono(codigo, numero, tipo, operadora);
+    }
+    
+    public void mostrarTelefonos(List<Telefono> telefonos){
+        for (Object telefono : telefonos) {
+            System.out.println("Telefonos: "+telefono+"\n");
+        }
+    }
+}
